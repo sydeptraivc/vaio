8f1d035cfa27ada1c39551d7bf273153f9455740
sydeptraivc <111412514+sydeptraivc@users.noreply.github.com>
2022-12-01 12:34:42 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,18 @@
+module.exports = {
+    //kanaĹ‚y
+    kanal: "922846121355804672",
+    proxyupdate: "922846121355804672",
+
+    //role
+    normal: "919982860482535484",
+    premium: "919982860482535484",
+    elite: "919332164980580352",
+    admin: "922845573806178396",
+
+    //ustawienia bota
+    token: "OTE5MzQ1MzkwMjMzNDU2NjYx.YbUdHQ.x-Twv1FZMaLzpapzbX0D7arv4nQ",
+    dev: ["880435061365809173"],
+    settings: {
+        prefix: "$"
+    }
+};
